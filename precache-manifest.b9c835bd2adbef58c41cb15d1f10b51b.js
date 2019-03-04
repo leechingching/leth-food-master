c28939c274d75da0381e778395fbdaa83e5d0b5d
Ching Ching Lee <allisonchinglee@gmail.com>
2019-03-04 22:04:40 +0800

    Updates

---
+++ precache-manifest.b9c835bd2adbef58c41cb15d1f10b51b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fef3d2201fcc7842eee6",
+    "url": "/leth-food-master/static/js/runtime~main.fef3d220.js"
+  },
+  {
+    "revision": "64b4ebbf0bef7b448e11",
+    "url": "/leth-food-master/static/js/main.64b4ebbf.chunk.js"
+  },
+  {
+    "revision": "7a89a0b74bb99e77b79e",
+    "url": "/leth-food-master/static/js/1.7a89a0b7.chunk.js"
+  },
+  {
+    "revision": "64b4ebbf0bef7b448e11",
+    "url": "/leth-food-master/static/css/main.b961d0af.chunk.css"
+  },
+  {
+    "revision": "e64fde389d804701ca1a0e17f847bb4a",
+    "url": "/leth-food-master/index.html"
+  }
+];
